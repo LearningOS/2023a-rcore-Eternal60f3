ea6cd6d7f49795a2567e3c20c862dba55cd158d9
Dream <2036019675@qq.com>
2023-11-19 14:50:10 +0800

    要提交的变更：       修改：     os/src/syscall/process.rs

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -81,7 +81,7 @@ pub fn sys_mmap(start: usize, len: usize, port: usize) -> isize {
     }
 
     let start_va: VirtAddr = start.into();
-    let end_va: VirtAddr = (start + len - 1).into();
+    let end_va: VirtAddr = (start + len).into();
     
     let start_vpn: VirtPageNum = start_va.into();
     let end_vpn: VirtPageNum = end_va.ceil().into();
@@ -110,7 +110,7 @@ pub fn sys_munmap(start: usize, len: usize) -> isize {
     }
 
     let start_va: VirtAddr = start.into();
-    let end_va: VirtAddr = (start + len - 1).into();
+    let end_va: VirtAddr = (start + len).into();
     
     let start_vpn: VirtPageNum = start_va.into();
     let end_vpn: VirtPageNum = end_va.ceil().into();
